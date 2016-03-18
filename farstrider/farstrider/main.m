f32621ce2a74d4e8086629b172ea32dfab1cc016
Jon Rubin <jb.rubin@gmail.com>
2016-03-18 00:09:02 -0400

    Stubs out a basic daemon, installed and loaded as part of the build phases.

---
+++ farstrider/farstrider/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  farstrider
+//
+//  Created by Jonathon Rubin on 3/17/16.
+//  Copyright © 2016 Jonathon Rubin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        NSLog(@"Running");
+        
+    }
+    return 0;
+}

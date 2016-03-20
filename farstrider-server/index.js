413c69ee47294a1664f722d5d949b311dc5c17cb
Jon Rubin <jb.rubin@gmail.com>
2016-03-20 19:08:21 -0400

    Basic post call for locations, storing them in mongo db.

--- farstrider-server/index.js
+++ farstrider-server/index.js
@@ -1,13 +1,57 @@
-var koa = require('koa');
-var route = require('koa-route');
+var koa = require('koa')
+var route = require('koa-route')
+var parse = require('co-body')
 
-var app = koa();
+var mongoose = require('mongoose')
+var Schema = mongoose.schema;
+var locationSchema = mongoose.Schema({
+	timestamp :Date,
+	latitude :Number,
+	longitude :Number
+})
+
+var koa_mongoose = require('koa-mongoose')
+var Location = mongoose.model('Location', locationSchema)
+
+var app = koa()
+
+app.use(koa_mongoose({
+	user: '',
+	pass: '',
+	host: '127.0.0.1',
+	port: 27017,
+	database :'locations',
+	db: {
+		native_parser: true
+	},
+	server: {
+		poolSize: 5
+	}
+	
+}))
 
 app.use(route.get('/', index));
+app.use(route.post('/api/v1/location/add/', addLocation))
+
 
 function *index() {
 	this.body = "<h1>farstrider-server</h1>";
 }
 
+function *addLocation() {
+	console.log("Hit addLocation: " + this)
+	var body = yield parse.json(this)
+	console.log("Adding location: " + JSON.stringify(body))
+	var location = new Location({
+		timestamp: body.timestamp,
+		latitude: body.latitude,
+		longitude: body.longitude
+	})
+	console.log("Storing in Mongo: +", location)
+	yield location.saveQ()
+	console.log("Added location")
+	this.body = 'OK'
+}
 
+console.log("listening")
 app.listen(8080);

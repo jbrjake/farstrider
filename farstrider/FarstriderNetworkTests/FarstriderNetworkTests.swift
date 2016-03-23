02f55d38fe21fb8e40c0f573820797b2f79e64a2
Jon Rubin <jb.rubin@gmail.com>
2016-03-23 01:21:22 -0400

    Defines a protocol for the network controller, with methods corresponding to the 2 server endpoints. They're both implemented with async NSURLSessions and both tested with a single add-and-read call that proves round trips work.

--- farstrider/FarstriderNetworkTests/FarstriderNetworkTests.swift
+++ farstrider/FarstriderNetworkTests/FarstriderNetworkTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class FarstriderNetworkTests: XCTestCase {
     
+    let network = FarstriderNetworkController()
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,16 +23,81 @@ class FarstriderNetworkTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func testAddAndGetLocation() {
+        let addExpectation = self.expectationWithDescription("Adding a location completes")
+        
+        let timestamp = NSDate()
+        let lon = Float(150.0)
+        let lat = Float(100.0)
+        network.addLocation(
+            atTime: timestamp, 
+            withLongitude: lon, 
+            andLatitude: lat) 
+        { 
+            addExpectation.fulfill()
         }
+        
+        self.waitForExpectationsWithTimeout(10, handler: nil)
+        
+        let getExpectation = self.expectationWithDescription("Locations are returned")
+        
+        network.getLocations() { locations in            
+            XCTAssertNotNil(
+                locations, 
+                "The location array should exist.");
+            
+            XCTAssertNotEqual(
+                locations?.count, 
+                0, 
+                "There should be at least one location.")
+            
+            let item = locations?.lastObject
+
+            XCTAssertNotNil(
+                item as? NSDictionary, 
+                "The item should be an NSDictionary")
+            
+            if let location = item as? NSDictionary {
+                
+                XCTAssertNotNil(
+                    location["timestamp"] as? String, 
+                    "The location should contain a timestamp value");
+
+                // And since now we know the timestamp exists and is a string, it's safe to...
+                let formatter = NSDateFormatter()
+                formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+                let returnedTimestamp = formatter.dateFromString(location["timestamp"] as! String)
+                
+                XCTAssertNotNil(
+                    location["latitude"] as? Float, 
+                    "The location should contain a latitude value");
+               
+                XCTAssertNotNil(
+                    location["longitude"] as? Float, 
+                    "The location should contain a longitude value");
+                
+
+                XCTAssertEqual(
+                    Int64((returnedTimestamp?.timeIntervalSince1970)! * 1000), 
+                    Int64(timestamp.timeIntervalSince1970 * 1000), 
+                    "The location timestamp should be the same as the one we just added, give or take a few ms")
+                
+                XCTAssertEqual(
+                    location["latitude"] as? Float, 
+                    lat, 
+                    "The latitude should be the same as the one we just added")
+                
+                XCTAssertEqual(
+                    location["longitude"] as? Float, 
+                    lon, 
+                    "The longitude should be the same as the one we just added.")
+            }
+            
+            getExpectation.fulfill()
+        }
+
+        self.waitForExpectationsWithTimeout(10, handler: nil)
+
     }
     
 }

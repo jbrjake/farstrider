c8ef7afa6446e151023256fed428964362a71095
Jon Rubin <jb.rubin@gmail.com>
2016-03-22 22:39:26 -0400

    Adds the stub of a FarstriderNetwork.framework to handle the network client for the server API. And embedded inside is a farstrider-network-service XPC service to manage access to it. The farstrider-agent will load this framework but only call upon the embedded network service.

---
+++ farstrider/farstrider-network-service/main.m
@@ -0,0 +1,49 @@
+//
+//  main.m
+//  farstrider-network-service
+//
+//  Created by Jonathon Rubin on 3/22/16.
+//  Copyright © 2016 Jonathon Rubin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "farstrider_network_service.h"
+
+@interface ServiceDelegate : NSObject <NSXPCListenerDelegate>
+@end
+
+@implementation ServiceDelegate
+
+- (BOOL)listener:(NSXPCListener *)listener shouldAcceptNewConnection:(NSXPCConnection *)newConnection {
+    // This method is where the NSXPCListener configures, accepts, and resumes a new incoming NSXPCConnection.
+    
+    // Configure the connection.
+    // First, set the interface that the exported object implements.
+    newConnection.exportedInterface = [NSXPCInterface interfaceWithProtocol:@protocol(farstrider_network_serviceProtocol)];
+    
+    // Next, set the object that the connection exports. All messages sent on the connection to this service will be sent to the exported object to handle. The connection retains the exported object.
+    farstrider_network_service *exportedObject = [farstrider_network_service new];
+    newConnection.exportedObject = exportedObject;
+    
+    // Resuming the connection allows the system to deliver more incoming messages.
+    [newConnection resume];
+    
+    // Returning YES from this method tells the system that you have accepted this connection. If you want to reject the connection for some reason, call -invalidate on the connection and return NO.
+    return YES;
+}
+
+@end
+
+int main(int argc, const char *argv[])
+{
+    // Create the delegate for the service.
+    ServiceDelegate *delegate = [ServiceDelegate new];
+    
+    // Set up the one NSXPCListener for this service. It will handle all incoming connections.
+    NSXPCListener *listener = [NSXPCListener serviceListener];
+    listener.delegate = delegate;
+    
+    // Resuming the serviceListener starts this service. This method does not return.
+    [listener resume];
+    return 0;
+}
